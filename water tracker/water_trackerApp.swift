0adb5496a0dc3d4bf85802ad3a2493960dfbb757
Haya Fahad <hayafahad46@gmail.com>
2023-10-08 01:31:47 +0300

    Initial Commit

---
+++ water tracker/water_trackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  water_trackerApp.swift
+//  water tracker
+//
+//  Created by Haya Fahad Alsabr on 23/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct water_trackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
